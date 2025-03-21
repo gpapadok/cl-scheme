0c3ec5c07b6639f5e7f96e9521880b66ce179c27
Giorgos Papadokostakis <gpapadok@Mac.home>
2025-03-22 00:25:22 +0200

    Remove useless clause

--- src/evaluate.lisp
+++ src/evaluate.lisp
@@ -51,13 +51,13 @@
 (defun lookup-variable (expr env) (env-lookup env expr))
 
 (defun evaluate (expr env)
-  (cond ((eq expr :env) (print env)) ; TODO: Remove
-        ((self-evaluating-p expr) expr)
-        ((special-form-p expr) (evaluate-special-form expr env))
-        ((variablep expr) (lookup-variable expr env))
-        ((applicationp expr) (evaluate-application expr env))
-        ((atom expr) expr)
-        (t (error "Unknown expression ~a" expr))))
+  (cond
+    ((self-evaluating-p expr) expr)
+    ((special-form-p expr) (evaluate-special-form expr env))
+    ((variablep expr) (lookup-variable expr env))
+    ((applicationp expr) (evaluate-application expr env))
+    ((atom expr) expr)
+    (t (error "Unknown expression ~a" expr))))
 
 (defun evaluate-body (body env)
   (let ((result (evaluate (car body) env)))
